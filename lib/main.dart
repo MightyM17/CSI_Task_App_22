e8f0cccd163d7b4d087bb48b4baebf32d4b9374d
Mighty <mightymb17@gmail.com>
2022-09-28 00:22:03 +0530

    main.dart: Added button

--- lib/main.dart
+++ lib/main.dart
@@ -8,7 +8,20 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.black45,
+      backgroundColor: Color(0xff202630),
+      body: Center(
+        child: TextButton(
+          onPressed: () {
+            print('you clicked me');
+          },
+          child: Text("Test"),
+          style: ButtonStyle(
+          shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+            RoundedRectangleBorder(borderRadius: BorderRadius.zero)
+          ),
+          ),
+        ),
+      ),
     );
   }
 }
